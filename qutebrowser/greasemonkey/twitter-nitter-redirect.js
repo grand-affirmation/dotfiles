90809c8b7bc248aabb81e531f443aca906b26f73
grand-affirmation <github@dta7.anonaddy.com>
2021-11-21 19:37:33 +0800

    homepage file; yt autoskip userscript; bash aliases

--- qutebrowser/greasemonkey/twitter-nitter-redirect.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name           twitter to nitter
-// @namespace      qutebrowser scripts
-// @match          http://twitter.com/*
-// @match          https://twitter.com/*
-// @run-at         document-start
-// ==/UserScript==
-
-location.href=location.href.replace("twitter.com","nitter.kavin.rocks");
