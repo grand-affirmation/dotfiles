f99c39ffab36574d053ee241109e07fccdedc60d
grand-affirmation <github@dta7.anonaddy.com>
2021-11-23 07:30:43 +0800

    modified qutebrowser userscript aria2c fetch

--- qutebrowser/userscripts/aria2c_fetch.sh
+++ qutebrowser/userscripts/aria2c_fetch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aria2c -s16 -x16 $QUTE_URL
+DIRECTORY=$(zenity --entry --text="Enter directory to download it" --title="Download Where?")
+alacritty --working-directory "$DIRECTORY" -e aria2c --seed-time=0 -s16 -x16 $QUTE_URL
